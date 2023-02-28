ffe0fdf3aa6d256ac2283ea11d1ea79f980af470
Ilya <yakubenko@complexis.biz>
2023-02-28 19:54:00 +0300

    task3

--- task3.py
+++ task3.py
@@ -1,5 +1,8 @@
-year = int(input('Введите год: '))
-if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
-    print("Год ", year, " - високосный")
-else:
-    print("Это год не високосный")
+def high_year(year):
+    # year = int(input('Введите год: '))
+    if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+       print("Год ", year, " - високосный")
+    else:
+      print("Это год не високосный")
+
+print(high_year(int(input('Введите год: '))))
